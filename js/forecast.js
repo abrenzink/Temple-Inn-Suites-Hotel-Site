9e6a803cfbcd7a206494a52d5571ccb38aa29e21
abrenzink <a.brenzink@gmail.com>
2022-04-05 14:00:05 -0400

    ---

--- js/forecast.js
+++ js/forecast.js
@@ -4,48 +4,44 @@ let cards=[];
 
 fetch(forecastURL).then(response => response.json()).then(jsObj => {
     const forecast = jsObj.list;
-    cards.push(forecast[0]);
-
-    let myDate = cards[0].dt_txt;
-    let today = new Date(myDate);
-    today = today.getDate();
-
-    getDays(today, cards)
+    cards = getDays(forecast);
     cards.forEach(createCard);
+});
 
-  });
+function getDays(array) {
+  const n = 3;
+  const sample = array
+  .map(x => ({ x, rand: Math.random() }))
+  .sort((a, b) => a.rand + b.rand)
+  .map(a => a.x)
+  .slice(0, n);
 
-function getDays(today, array) {
-
-    const n = 3;
-    let sample = array.sort((today, b) => a + b);
-    sample.map(a => a.x).slice(0, n);
-
-    return sample;
-  }
+  return sample;
+}
 
 function createCard (day){
+  console.log(day)
     let card = document.createElement('section');
     card.className = 'spotCard';
 
-    let date = document.createElement('span');
-    date.textContent = day.dt_txt;
-
     let iconsrc = document.createElement('img');
-    iconsrc.setAttribute('src', `https://openweathermap.org/img/w/${day.weather.icon}.png`);
+    iconsrc.setAttribute('src', `https://openweathermap.org/img/w/${day.weather[0].icon}.png`);
     iconsrc.setAttribute('alt', "");
 
+    let date = document.createElement('span');
+    parseDate = new Date(`${day.dt_txt}`);
+    date.textContent = parseDate;
+
     let temp = document.createElement('span');
-    temp.textContent= day.main.temp;
+    temp.innerHTML= `<br>Temperature: ${day.main.temp}</br>`
 
     let weatherDesc = document.createElement('span');
-    weatherDesc.textContent = day.description;
+    weatherDesc.innerHTML = `<br>${day.weather[0].description}</br>`;
 
     card.appendChild(iconsrc);
     card.appendChild(date);
     card.appendChild(temp);
     card.appendChild(weatherDesc);
-
 
     let section = document.querySelector('.forecast');
     section.appendChild(card);
